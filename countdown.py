9fdfea46fb2f5cade7bceaa647cfc520157a2f63
edward <edwardw@qq.com>
2016-07-26 12:07:41 +0800

    add test_version

--- countdown.py
+++ countdown.py
@@ -3,7 +3,7 @@
 # @Author: edward
 # @Date:   2016-07-21 15:52:58
 # @Last Modified by:   edward
-# @Last Modified time: 2016-07-26 12:04:56
+# @Last Modified time: 2016-07-26 12:07:09
 import wx
 import ui
 from resource import rat_head_original
@@ -26,7 +26,7 @@ class App(wx.App):
 
         if self.test_version:
             fr.SetTitle(fr.GetTitle() + '(test_version)')
-            if test_version.expired(2016,7,15):
+            if test_version.expired(2016,8,15):
                 wx.MessageBox('expired!')
                 fr.Destroy()
         self.SetTopWindow(fr)
