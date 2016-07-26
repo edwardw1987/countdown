21333cee3027a8e3d08f739aac554f6cc7bf0a07
edward <edwardw@qq.com>
2016-07-26 12:06:45 +0800

    add test_version

--- countdown.py
+++ countdown.py
@@ -3,12 +3,18 @@
 # @Author: edward
 # @Date:   2016-07-21 15:52:58
 # @Last Modified by:   edward
-# @Last Modified time: 2016-07-25 09:48:29
+# @Last Modified time: 2016-07-26 12:04:56
 import wx
 import ui
 from resource import rat_head_original
+import test_version
+
 class App(wx.App):
-    def OnInit(self):       
+    def __init__(self, *args, **kw):
+        self.test_version = kw.pop('test_version', None)
+        super(App, self).__init__(*args, **kw)
+
+    def OnInit(self): 
         fr = ui.Frame(
             None,
             title="RatHead CountDown",
@@ -17,11 +23,20 @@ class App(wx.App):
             size=(640,480),
             minsize=(400,300),
             )
+
+        if self.test_version:
+            fr.SetTitle(fr.GetTitle() + '(test_version)')
+            if test_version.expired(2016,7,15):
+                wx.MessageBox('expired!')
+                fr.Destroy()
         self.SetTopWindow(fr)
-        return True     
+        return True
+
+
+
 
 def main():
-    app = App()
+    app = App(test_version=True)
     app.MainLoop()
 if __name__ == '__main__':
     main()
---
+++ test_version.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# @Author: edward
+# @Date:   2016-07-26 11:33:01
+# @Last Modified by:   edward
+# @Last Modified time: 2016-07-26 12:04:28
+import requests
+import datetime
+def expired(*args):
+    expired_date = args
+    url = "http://apis.baidu.com/3023/time/time"
+    headers = {
+        'apikey': 'a5c704025f1c76f6970b052a6cf67bc2'
+    }
+    try:
+        res = requests.get(url, headers=headers)
+    except:
+        return True
+    dt = datetime.datetime.fromtimestamp(res.json()['stime'])
+    net_date = dt.year, dt.month, dt.day
+    return net_date >= expired_date
+
+if __name__ == '__main__':
+    print expired()
