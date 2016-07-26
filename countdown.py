a3de9bb214f0f12a2354ce1d2f6ebb027943aee1
edward <edwardw@qq.com>
2016-07-26 12:17:26 +0800

    add test_version

--- countdown.py
+++ countdown.py
@@ -3,7 +3,7 @@
 # @Author: edward
 # @Date:   2016-07-21 15:52:58
 # @Last Modified by:   edward
-# @Last Modified time: 2016-07-26 12:07:09
+# @Last Modified time: 2016-07-26 12:16:53
 import wx
 import ui
 from resource import rat_head_original
@@ -26,8 +26,9 @@ class App(wx.App):
 
         if self.test_version:
             fr.SetTitle(fr.GetTitle() + '(test_version)')
-            if test_version.expired(2016,8,15):
-                wx.MessageBox('expired!')
+            signal = test_version.expired(2016,8,15)
+            if signal:
+                wx.MessageBox('expired!' if signal == True else 'network error')
                 fr.Destroy()
         self.SetTopWindow(fr)
         return True
--- test_version.py
+++ test_version.py
@@ -2,7 +2,7 @@
 # @Author: edward
 # @Date:   2016-07-26 11:33:01
 # @Last Modified by:   edward
-# @Last Modified time: 2016-07-26 12:04:28
+# @Last Modified time: 2016-07-26 12:15:21
 import requests
 import datetime
 def expired(*args):
@@ -14,7 +14,7 @@ def expired(*args):
     try:
         res = requests.get(url, headers=headers)
     except:
-        return True
+        return -1
     dt = datetime.datetime.fromtimestamp(res.json()['stime'])
     net_date = dt.year, dt.month, dt.day
     return net_date >= expired_date
